cbc19ac212176e4d4402a85d6990cacf09e35eb7
Lepaulski <zzqiuzz@gmail.com>
2025-05-18 09:23:56 +0000

    add online softmax

--- kernels/softmax.cpp
+++ kernels/softmax.cpp
@@ -56,29 +56,64 @@ vector<float> run_online_softmax(const vector<float> &in_data){
         res[i] = exp(in_data[i] - max_value) / sum_value;
 
     return res;
-
-
 }
 
-int main(){
-    vector<float> data = {0.23, 0.56, 0.85, 0.12, 0.01};
-    for(auto _data: data)
-        cout << _data << " ";
-    cout << endl;
+float run_online_softmax_default_dot_product(const vector<float> &in_data, const vector<float> &value){ 
+    vector<float> res;
+    float dot_res = 0.f;
+    res.resize(in_data.size());
+    float max_value = -std::numeric_limits<float>::infinity();
+    float sum_value = 0.f, pre_max_value = 0.f;
+    for(int i = 0; i < in_data.size(); i++){
+        max_value = std::max(max_value, in_data[i]);
+        sum_value = sum_value * exp(pre_max_value - max_value) + exp(in_data[i] - max_value);
+        pre_max_value = max_value;
+    }
+    for(int i = 0; i < in_data.size(); i++)
+        dot_res += exp(in_data[i] - max_value) / sum_value * value[i]; // 这里的for循环能否和上面的合并来求出res
 
-    // run_naive_softmax(data);
-    vector<float> res0 = run_naive_softmax(data);
-    for(auto _data: res0)
+    return dot_res;
+}
+
+float run_online_softmax_optimal_dot_product(const vector<float> &in_data, const vector<float> &value){ 
+    vector<float> res;
+    float dot_res = 0.f;
+    res.resize(in_data.size());
+    float max_value = -std::numeric_limits<float>::infinity();
+    float sum_value = 0.f, pre_max_value = 0.f, pre_sum = 0.f;
+    float pre_res_value = 0.f;
+    for(int i = 0; i < in_data.size(); i++){
+        max_value = std::max(max_value, in_data[i]);
+        sum_value = sum_value * exp(pre_max_value - max_value) + exp(in_data[i] - max_value);
+        dot_res = dot_res * pre_sum * exp(pre_max_value - max_value) / sum_value + exp(in_data[i] - max_value) / sum_value * value[i]; 
+        pre_max_value = max_value;
+        pre_sum = sum_value;
+    }
+ 
+    return dot_res;
+}
+
+void print_vector(vector<float> res){
+    for(auto _data: res)
         cout << _data << " ";
     cout << endl;
-    vector<float> res1 = run_safe_softmax(data);
-    for(auto _data: res1)
-        cout << _data << " ";
-    cout << endl;
-    vector<float> res2 = run_online_softmax(data);
+}
+int main(){
+    vector<float> data = {2.3, 5.6, 8.5, 1.2, 0.1};
+    vector<float> value{1.1, 2.2, 3.3, 4.4, 5.5};
+    print_vector(data);
+ 
+    vector<float> res0 = run_naive_softmax(data); 
+    vector<float> res1 = run_safe_softmax(data); 
+    vector<float> res2 = run_online_softmax(data); 
+    float res3 = run_online_softmax_default_dot_product(data, value); 
+    float res4 = run_online_softmax_optimal_dot_product(data, value); 
     
-    for(auto _data: res2)
-        cout << _data << " ";
-    cout << endl;
+    print_vector(res0);
+    print_vector(res1);
+    print_vector(res2);
+    cout << "default dot product: " << res3 << endl;
+    cout << "optimal dot product: " << res4 << endl; 
+
     return 0;
 }
